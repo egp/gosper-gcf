1b4d85829900227ddf577eb2557b2b2b1821853d
Edward G Prentice <egp@egp.net>
2026-03-25 21:51:06 -0400

    add pending test square(sqrt2))

--- named/square_sqrt2_bb_test.go
+++ named/square_sqrt2_bb_test.go
@@ -1,45 +1,60 @@
-// named/square_sqrt2_bb_test.go v2
+// named/square_sqrt2_bb_test.go v1
 package named_test
 
-// func TestBB_SquareOfSqrt2IsExactlyTwo(t *testing.T) {
-// 	g := core.Square(named.Sqrt2())
+import (
+	"math/big"
+	"testing"
+	"time"
 
-// 	term, status := nextRCFWithTimeout(t, g, time.Second)
-// 	if status != core.StatusOK {
-// 		t.Fatalf("first status = %v, want %v", status, core.StatusOK)
-// 	}
-// 	if term.A().Cmp(big.NewInt(2)) != 0 {
-// 		t.Fatalf("first term = %v, want 2", term.A())
-// 	}
+	"github.com/egp/gosper-gcf/core"
+	"github.com/egp/gosper-gcf/named"
+)
 
-// 	_, eofStatus := nextRCFWithTimeout(t, g, time.Second)
-// 	if eofStatus != core.StatusEOF {
-// 		t.Fatalf("EOF status = %v, want %v", eofStatus, core.StatusEOF)
-// 	}
-// }
+const pendingTestSquareOfSqrt2 = true
 
-// func nextRCFWithTimeout(t *testing.T, g *core.GCF, timeout time.Duration) (core.RCFTerm, core.Status) {
-// 	t.Helper()
+func TestBB_SquareOfSqrt2IsExactlyTwo(t *testing.T) {
+	if pendingTestSquareOfSqrt2 {
+		t.Skip("pending DLFT support for infinite algebraic streams under current PQStream+Range contract")
+	}
 
-// 	type result struct {
-// 		term   core.RCFTerm
-// 		status core.Status
-// 	}
+	g := core.Square(named.Sqrt2())
 
-// 	ch := make(chan result, 1)
+	term, status := nextRCFWithTimeout(t, g, time.Second)
+	if status != core.StatusOK {
+		t.Fatalf("first status = %v, want %v", status, core.StatusOK)
+	}
+	if term.A().Cmp(big.NewInt(2)) != 0 {
+		t.Fatalf("first term = %v, want 2", term.A())
+	}
 
-// 	go func() {
-// 		term, status := g.NextRCF()
-// 		ch <- result{term: term, status: status}
-// 	}()
+	_, eofStatus := nextRCFWithTimeout(t, g, time.Second)
+	if eofStatus != core.StatusEOF {
+		t.Fatalf("EOF status = %v, want %v", eofStatus, core.StatusEOF)
+	}
+}
 
-// 	select {
-// 	case res := <-ch:
-// 		return res.term, res.status
-// 	case <-time.After(timeout):
-// 		t.Fatalf("NextRCF() did not complete within %v", timeout)
-// 		return core.NewRCFTerm(nil), core.StatusInvalidInput
-// 	}
-// }
+func nextRCFWithTimeout(t *testing.T, g *core.GCF, timeout time.Duration) (core.RCFTerm, core.Status) {
+	t.Helper()
 
-// named/square_sqrt2_bb_test.go v2
+	type result struct {
+		term   core.RCFTerm
+		status core.Status
+	}
+
+	ch := make(chan result, 1)
+
+	go func() {
+		term, status := g.NextRCF()
+		ch <- result{term: term, status: status}
+	}()
+
+	select {
+	case res := <-ch:
+		return res.term, res.status
+	case <-time.After(timeout):
+		t.Fatalf("NextRCF() did not complete within %v", timeout)
+		return core.NewRCFTerm(nil), core.StatusInvalidInput
+	}
+}
+
+// named/square_sqrt2_bb_test.go v1
